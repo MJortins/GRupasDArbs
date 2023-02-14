69866bf1ccdaadcbdb62a51157e019fcebaadae5
Sugnibo Music <123469865+MJortins@users.noreply.github.com>
2023-02-14 12:41:17 +0000

    matematik

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ roll_width = float(input("Ieraksti linoleja platumu (m): "))
 room_width = float(input("ieraksti istabas platumu (m): "))
 room_length = float(input("ieraksti istabas garumu (m): "))
 
+
+linoleum_area = roll_width * (room_width + 0.1) * (room_length + 0.1)  
+total_cost = linoleum_area * linoleum_price
+
+
