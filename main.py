2076d41577d2ee7217e9e99dc03220731392e21e
denissp <123469959+denisspetrovs10@users.noreply.github.com>
2023-02-14 12:40:51 +0000

    pievienoju vel 2 rindas inputus

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 linoleum_price = float(input("Ieraksti linoleja cenu (EUR/m2): "))
 roll_width = float(input("Ieraksti linoleja platumu (m): "))
+room_width = float(input("ieraksti istabas platumu (m): "))
+room_length = float(input("ieraksti istabas garumu (m): "))
+
